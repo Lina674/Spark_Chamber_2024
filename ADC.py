4c2282a7e47f81751328839e09523fd9426f8532
Lina Guezi <68437558+Lina674@users.noreply.github.com>
2024-04-07 18:58:41 -0400

    Update ADC.py

--- ADC.py
+++ ADC.py
@@ -6,4 +6,7 @@ def read_analog_values():
     # Read analog values from channels 0 and 1
     analog_value_1 = adc.value  # Channel 0
     analog_value_2 = adc.channel[1].value  # Channel 1
-    return analog_value_1, analog_value_2
+    
+    voltage_1 = analog_value_1 * 3.3 #Vref on the ADC
+    voltage_2 = analog_value_2 * 3.3
+    return voltage_1, voltage_2
